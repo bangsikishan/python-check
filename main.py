706e6860c3de3ec4612f5ccdc2e965db087c14c6
KM <bangsikishan@duck.com>
2023-12-08 16:07:12 +0545

    Updated code

--- main.py
+++ main.py
@@ -14,8 +14,15 @@ chromedriver_path = "D:\\Python\\scrape\\chromedriver-win64\\chromedriver.exe"
 service = Service(executable_path=chromedriver_path)
 
 options = Options()
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36")
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-extensions")
+options.add_argument("--disable-notifications")
+options.add_argument("--disable-popup-blocking")
 options.add_argument("--log-level=3")
-# options.add_argument("--headless")
+options.add_argument("--headless")
+
 
 # Load website data
 with open("test_websites_list.json", "r") as f:
@@ -24,7 +31,7 @@ with open("test_websites_list.json", "r") as f:
 # Initialize WebDriver
 driver = webdriver.Chrome(options=options, service=service)
 
-wait = WebDriverWait(driver, 20)
+wait = WebDriverWait(driver, 30)
 
 def check_website(selector, selector_value, xpath):
     parent_element = wait.until(
@@ -52,6 +59,8 @@ def get_selector(selector):
 results = {}
 ecgains = []
 for key, value in websites_list.items():
+    print(f"[+] Trying website {key} out of {len(websites_list)}...", end="\r")
+
     try:
         driver.get(value["url"])
     except WebDriverException:
@@ -63,7 +72,9 @@ for key, value in websites_list.items():
             value["primary_selector_value"],
             value["primary_xpath"]
         )
-    except NoSuchElementException:
+    except (NoSuchElementException, TimeoutException):
+        # with open("source.html", "w", encoding="utf-8") as f:
+        #     f.write(driver.page_source)
         text = check_website(
             get_selector(value["secondary_selector"]),
             value["secondary_selector_value"],
@@ -75,8 +86,12 @@ for key, value in websites_list.items():
         ecgains.append(value["ecgain"])
     else:
         results[key] = {"ecgain": value["ecgain"], "contains_bids": True}
-
+        value["message"] = text.replace("\n", "\n")
+print("")
 driver.quit()
+
+with open("test_websites_list.json", "w") as f:
+    json.dump(websites_list, f, indent=4)
 
 with open("results.json", "w") as f:
     json.dump(results, f, indent=4)
